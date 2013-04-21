99f7fb009e40be40f56c19b5e840f0bf7cf9f775
David Y. Gonzalez <dvdyzag@gmail.com>
2013-04-20 18:58:20 -0600

    Proyecto ha comenzado

---
+++ src/inventario/Inventario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventario;
+
+/**
+ *
+ * @author Yo
+ */
+public class Inventario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        principal p= new principal();
+        p.setVisible(true);
+    }
+}
